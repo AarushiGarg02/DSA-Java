7d4a1f7139cce79b1d56483897d770b3192ee5b8
AarushiGarg02 <aarushigarg56@gmail.com>
2026-01-13 17:18:19 +0530

    Added first array problem - SecondLargestNumber

---
+++ Arrays/SecondLargestNumber.java
@@ -0,0 +1,32 @@
+//Problem: Return Second Largest Element
+//Time Complexity: O(n)
+//Auxiliary Space: O(1)
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        /*
+        Algorithm:
+        1. initialize int max and secondmax
+        2. traverse array
+        3. compare every element with each other, and update the value
+        4. avoid duplicate values
+        5. return secondmax value
+        */
+       int max = Integer.MIN_VALUE;
+       int secondMax = Integer.MIN_VALUE;
+       for(int i =0; i < arr.length; i++){
+           if(arr[i] > max){
+               secondMax = max;
+               max = arr[i];
+           }
+           else if( arr[i] > secondMax && arr[i]!= max){
+               secondMax = arr[i];
+           }
+       }
+       return secondMax;
+    }
+    public static void main(String args[]){
+        int arr[] ={12, 35, 1, 10, 34, 1};
+        System.out.println(new Solution().getSecondLargest(arr));
+    }
+}
